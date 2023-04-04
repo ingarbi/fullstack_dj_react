6b5d183468b9fa9b2a92df33ea74b5683bff6a77
vboxuser <vboxuser@LinuxUbuntu.myguest.virtualbox.org>
2023-04-04 13:35:07 +0300

    ch6_setup ReactJs

---
+++ react_front/social-media-app/src/App.js
@@ -0,0 +1,13 @@
+import Home from "./pages/Home";
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  );
+}
+
+export default App;
